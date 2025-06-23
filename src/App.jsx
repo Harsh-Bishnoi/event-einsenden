924344c7751abb35c08d631be99cf0bcfa693c1b
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-06-23 09:18:56 +0530

    add hard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import './App.css'
 import Event from './components/Event'
 import ExploreWorld from './components/ExploreWorld'
+import Hard from './components/Hard'
+import { Routes, Route, Navigate, BrowserRouter } from 'react-router-dom';
 
 function App() {
 
@@ -8,6 +10,13 @@ function App() {
     <>
       <Event />
       <ExploreWorld />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Navigate to="/category/soft" />} />
+          <Route path="/category/:type" element={<Hard />} />
+          <Route path="/alphabet/:alphabet_name" element={<Hard />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
